ceacadc1f5b6f4d94c64b33f0d9a37c985578001
Ryan <hudsonry@pascack.org>
2019-02-04 11:23:02 -0500

    added page 2

--- deploy.py
+++ deploy.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
 def home():
    return render_template("index.html")
+
+@app.route("/page2")
+def page_2():
+   return render_template("page2.html")
+
 if __name__ == "__main__":
    app.run(debug=True)
